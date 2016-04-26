724a5d4e8a20085df56ff2d3b96f9de37ec14ad8
nguyenhuearmy <huearmynguyen@gmail.com>
2016-04-26 09:33:25 +0000

    add movie in controller, index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,23 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    
+    if params[:ratings]
+      @movies = Movie.where(rating: params[:ratings].keys)
+    end
+    
+    case params[:sort]
+    when 'title'
+      @movies = Movie.order('title ASC')
+      @title_hilite = 'hilite'
+    when 'release'
+      @releases = Movie.order('release ASC')
+      @release_hilite = 'hilite'
+    else
+      params[:ratings] ? @movies = Movie.where(rating: params[:ratings].keys) :
+                         @movies = Movie.all
+    end
   end
 
   def new
